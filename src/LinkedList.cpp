a1725d4e6df100b1460669cf951c8e52552f2eee
Parsa <parsaes1386@gmail.com>
2026-07-02 20:36:01 +0330

    did a basic main loop for the cli and started the implementation of the bidirectional linked list

---
+++ src/LinkedList.cpp
@@ -0,0 +1,25 @@
+#include <string>
+
+struct Node
+{
+    Node *prev;
+    Node *next;
+    string value;
+};
+
+class LinkedList
+{
+private:
+    /* data */
+public:
+    LinkedList(/* args */);
+    ~LinkedList();
+};
+
+LinkedList::LinkedList(/* args */)
+{
+}
+
+LinkedList::~LinkedList()
+{
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,78 @@
 #include <iostream>
+#include <string>
 
-int main(){
+#include <HashMap.h>
+
+using namespace std;
+
+enum COMMAND
+{
+    SET,
+    GET,
+    DEL,
+    EXISTS,
+    EXPIRE,
+    TTL
+};
+
+COMMAND Transform(string cmd)
+{
+    if (cmd == "SET")
+        return SET;
+    if (cmd == "GET")
+        return GET;
+    if (cmd == "DEL")
+        return DEL;
+    if (cmd == "EXISTS")
+        return EXISTS;
+    if (cmd == "EXPIRE")
+        return EXPIRE;
+    if (cmd == "TTL")
+        return TTL;
+}
+
+int main()
+{
+    do
+    {
+        string cmd;
+        string key;
+        cin >> cmd >> key;
+        COMMAND command = Transform(cmd);
+        switch (command)
+        {
+        case SET:
+        {
+            string value;
+            cin >> value;
+            break;
+        }
+        case GET:
+        {
+            break;
+        }
+        case DEL:
+        {
+            break;
+        }
+        case EXISTS:
+        {
+            break;
+        }
+        case EXPIRE:
+        {
+            break;
+        }
+        case TTL:
+        {
+            break;
+        }
+
+        default:
+            cout << "Unknown Command" << '\n';
+            break;
+        }
+    } while (1);
+
     return 0;
 }
