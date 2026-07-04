11d4b02a806a1f78fb9080d23e1b5ef0cb79b86c
Parsa <parsaes1386@gmail.com>
2026-07-04 11:28:45 +0330

    created the HashNode struct a simple link list specially for the HashMap then added the basic functions of the HashMap the TTL and expire time are remaining. need to add a new field to the struct for TTL

--- src/HashMap.cpp
+++ src/HashMap.cpp
@@ -1,17 +1,30 @@
 #include <string>
-#include <LinkedList.h>
+#include <iostream>
+
+struct HashNode
+{
+    std::string key, value;
+    HashNode *next;
+
+    HashNode(std::string k, std::string v) : key(k), value(v), next(nullptr) {}
+};
 
 class HashMap
 {
 private:
     static const int CAPACITY = 100009;
-    LinkedList arr[CAPACITY];
+    HashNode *arr[CAPACITY] = {nullptr};
 
     int getHash(std::string key);
 
 public:
     HashMap();
     ~HashMap();
+
+    void set(std::string key, std::string value);
+    void get(std::string key);
+    void del(std::string key);
+    void exists(std::string key);
 };
 
 HashMap::HashMap()
@@ -29,5 +42,102 @@ int HashMap::getHash(std::string key)
     {
         hash = hash * 33 + c;
     }
-    return hash & CAPACITY;
+    return hash % CAPACITY;
+}
+
+void HashMap::set(std::string key, std::string value)
+{
+    int hash = getHash(key);
+    if (arr[hash] == nullptr)
+    {
+        arr[hash] = new HashNode(key, value);
+        return;
+    }
+    HashNode *tmp = arr[hash];
+    while (tmp != nullptr)
+    {
+        if (tmp->key == key)
+        {
+            tmp->value = value;
+            return;
+        }
+        tmp = tmp->next;
+    }
+    HashNode *node = new HashNode(key, value);
+    node->next = arr[hash];
+    arr[hash] = node;
+}
+
+void HashMap::get(std::string key)
+{
+    int hash = getHash(key);
+    if (arr[hash] == nullptr)
+    {
+        std::cout << "Key Not Found" << '\n';
+        return;
+    }
+
+    HashNode *tmp = arr[hash];
+    while (tmp != nullptr)
+    {
+        if (tmp->key == key)
+        {
+            std::cout << tmp->value << '\n';
+            return;
+        }
+        tmp = tmp->next;
+    }
+    std::cout << "Key Not Found" << '\n';
+}
+
+void HashMap::del(std::string key)
+{
+    int hash = getHash(key);
+    if (arr[hash] == nullptr)
+    {
+        std::cout << "Key Not Found" << '\n';
+        return;
+    }
+    if (arr[hash]->key == key)
+    {
+        HashNode *tmp = arr[hash];
+        arr[hash] = arr[hash]->next;
+        delete (tmp);
+        return;
+    }
+
+    HashNode *tmp = arr[hash];
+    while (tmp->next != nullptr)
+    {
+        if (tmp->next->key == key)
+        {
+            HashNode *node = tmp->next;
+            tmp->next = tmp->next->next;
+            delete (node);
+            return;
+        }
+        tmp = tmp->next;
+    }
+}
+
+void HashMap::exists(std::string key)
+{
+    int hash = getHash(key);
+    if (arr[hash] == nullptr)
+    {
+        std::cout << "0" << '\n';
+        return;
+    }
+
+    HashNode *tmp = arr[hash];
+    while (tmp != nullptr)
+    {
+        if (tmp->key == key)
+        {
+            std::cout << 1 << '\n';
+            return;
+        }
+        tmp = tmp->next;
+    }
+    std::cout << "0" << '\n';
 }
